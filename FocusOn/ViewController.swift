3e6ca2b9df2c9a55598b3c89fecd35dfcf0d424c
mikegresham <51673033+mikegresham@users.noreply.github.com>
2020-05-02 17:56:20 +0100

    Initial Commit

---
+++ FocusOn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FocusOn
+//
+//  Created by Michael Gresham on 02/05/2020.
+//  Copyright © 2020 Michael Gresham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
